1fcf4be227d3f2a9752dacf8e3654f26d735ef9e
reaper <gabrieljaivona@gmail.com>
2025-05-12 22:42:13 -0400

    criando interface

---
+++ src/components/CitySelector.tsx
@@ -0,0 +1,23 @@
+
+{/*
+interface City {
+    id: string;
+    name: string;
+}
+
+interface NavbarProps {
+    cities: City[];
+    selectedCityId: string | null;
+    onCityChange: (cityId: string | null) => void;
+    activePage: Page;
+    onNavigate: (page: Page) => void;
+}
+*/}
+
+function CitySelector() {
+  return (
+    <div>CitySelector</div>
+  )
+}
+
+export default CitySelector
