5e5977c504473cd91b54419975fb0856d43f3722
reaper <gabrieljaivona@gmail.com>
2025-05-12 22:08:17 -0400

    Criando o componente de selecao de cidades

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,44 @@ import { NavLink } from "react-router-dom";
 
 
 function Header() {
+  const baseLink = "text-[#6D4C41] hover:text-[#4CAF50] hover:ease-linear";
+  const activeLink = "font-bold";
+
   return (
-    <nav>
-        <div className="flex">
-            <a href="#">Feira Aí</a>
-            <div>
-              <NavLink to="/Home">Início</NavLink>
-              <NavLink to="/Products">Produtos</NavLink>
-              <NavLink to="/MyOrders">Meus Pedidos</NavLink>
-              <NavLink to="/Contact">Contato / Ajuda</NavLink>
-              <NavLink to="/Auth">
+    <nav className="bg-[#FFF8E1] shadow">
+        <div className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3">
+            <NavLink to="/Home" className="text-[#4CAF50] font-bold text-2xl">Feira Aí</NavLink>
+            
+            <div className="flex gap-5">
+              <NavLink to="/Home" className={({ isActive }) =>
+                `${baseLink} ${isActive ? activeLink : ""}`}>
+                Inicío
+              </NavLink>
+
+              <NavLink to="/Products" className={({ isActive }) =>
+                `${baseLink} ${isActive ? activeLink : ""}`}>
+                Produtos
+              </NavLink>
+
+              <NavLink to="/MyOrders" className={({ isActive }) =>
+                `${baseLink} ${isActive ? activeLink : ""}`}>
+                Meus Pedidos
+              </NavLink>
+
+              <NavLink to="/Contact" className={({ isActive }) =>
+              `${baseLink} ${isActive ? activeLink : ""}`}>
+                Contato / Ajuda
+                </NavLink>
+
+              <NavLink to="/Auth" className={({ isActive }) =>
+              `${baseLink} ${isActive ? activeLink : ""}`}>
                 <button type="button">Entrar / Cadastrar</button>
               </NavLink>
+            </div>
+            <div>
+              <button>
+                
+              </button>
             </div>
         </div>
     </nav>
